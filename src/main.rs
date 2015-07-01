422142848aea80bff8bfa815b8b5fe46b9e6360e
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-02 01:27:58 +0530

    added basics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+struct Philosopher {
+	name: String,
+}
+
+impl Philosopher {
+	fn new(name: &str) -> Philosopher {
+		Philosopher {
+			name: name.to_string(),
+		}
+	}
+}
+
+
 fn main(){
-	println!("this is a compile test");
+	let ph1 = Philosopher::new("Acharya Vamana");
+	let ph2 = Philosopher::new("Maha Kapphina");
+	let ph3 = Philosopher::new("Gargi Vachaknavi");
+	let ph4 = Philosopher::new("Vacaspati Misra");
+	let ph5 = Philosopher::new("Adi Shankra");
+
 }
 
